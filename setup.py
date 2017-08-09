7dd80d7fb146f942eb0660dfc02d846cd9003f18
Michael Brown <mbrown@fensystems.co.uk>
2017-08-09 14:02:20 +0100

    [setup] Add basic setup.py script
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import textwrap
+from setuptools import setup
+
+setup(
+    name="systemcloud",
+    version="0.1",
+    license="GPLv2+",
+    author="Michael Brown",
+    author_email="mbrown@fensystems.co.uk",
+    description="OCF resource agents for building clustered systemd services",
+    long_description=textwrap.dedent("""
+    systemcloud provides a Python framework for building OCF resource
+    agents, and a set of OCF resource agents for managing distributed
+    services as part of a pacemaker+corosync cluster.
+    """).lstrip(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved '
+        ':: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Systems Administration',
+    ],
+    packages=['ocf', 'systemcloud'],
+)
