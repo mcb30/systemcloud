8c3fb30088b699e097ce239bc56c405322c64353
Seth Tunstall <seth@tunstall.in>
2021-03-02 15:10:34 +0000

    concourse-ci WIP

--- .concourse/centos/buildrpm.sh
+++ .concourse/centos/buildrpm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 # Cargo-culted and adapted from https://github.com/drahnr/oregano
 set -x
 set -e
@@ -13,9 +13,8 @@ cp -v systemcloud.spec ${RPMBUILD_DIR}/SPECS/
 #cp -v oregano*.tar.xz ${RPMBUILD_DIR}/SOURCES/
 
 cd ${RPMBUILD_DIR}
-echo rpmbuild --define "_topdir %(pwd)" --define "_builddir %{_topdir}/BUILD" --define "_rpmdir %{_topdir}/RPMS" --define "_srcrpmdir %{_topdir}/SRPMS" --define "_specdir %{_topdir}/SPECS" --define "_sourcedir  %{_topdir}/SOURCES" 
+echo rpmbuild --define "_topdir %(pwd)" --define "_builddir %{_topdir}/BUILD" --define "_rpmdir %{_topdir}/RPMS" --define "_srcrpmdir %{_topdir}/SRPMS" --define "_specdir %{_topdir}/SPECS" --define "_sourcedir  %{_topdir}/SOURCES" -ba SPECS/systemcloud.spec
 
--ba SPECS/systemcloud.spec
 rpmbuild \
 --define "_topdir %(pwd)" \
 --define "_builddir %{_topdir}/BUILD" \
